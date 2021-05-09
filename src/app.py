caeae6b8b9f8731cb664893d6b2ebbbfef079d9f
steelpwner <molinaestren@gmail.com>
2021-05-09 02:22:17 -0500

    Calculadora flask

---
+++ src/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def home():
+    return "<h1>Hello world</h1>"
+
+@app.route("/calculadora",methods=["GET"])
+def calculadora():
+    return render_template("calculadora.html")
+
+@app.route("/calculadora",methods=["POST"])
+def calculo():
+    args = request.form
+    n1 = args['n1']
+    n2 = args['n2']
+    operacion = args['operacion']
+    return render_template("calculadora.html",resultado=calcular_operacion(n1,n2,operacion))
+
+def calcular_operacion(n1,n2,operacion):
+    if verificar_numero(n1) and verificar_numero(n2):
+        n1 = float(n1)
+        n2 = float(n2)
+        if operacion == "+":
+            return n1+n2
+        elif operacion == "-":
+            return n1-n2
+        elif operacion == "*":
+            return n1*n2
+        elif operacion == "/":
+            return n1/n2
+        else:
+            return "Operación inválida"
+    else:
+        return "Error en digitación de números"
+
+def verificar_numero(x):
+    try:
+        float(x)
+        return True
+    except:
+        return False
+
+app.run(port=5000,host="0.0.0.0",debug=True)
